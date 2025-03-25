cb4701f24d1c1406b78d2c3bc8d30bbdfd03b27b
Christian <chrise79456@gmail.com>
2025-03-25 09:13:24 +0100

    first commit

---
+++ src/test/java/com/BlueAlastor/task/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BlueAlastor.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
