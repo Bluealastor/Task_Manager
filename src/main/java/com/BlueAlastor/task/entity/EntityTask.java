c5ccf066e9f1bba12bf96e4c1be2c5c51ac115ec
Christian <chrise79456@gmail.com>
2025-03-25 09:19:41 +0100

    add entity Task

---
+++ src/main/java/com/BlueAlastor/task/entity/EntityTask.java
@@ -0,0 +1,24 @@
+package com.BlueAlastor.task.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class EntityTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+}
